fca434081ab114740e071487d915f1266c3014fd
Thomas Faller <tfaller1@gmx.de>
2021-01-17 15:30:33 +0100

    Return empty document if it does not exists.

--- cmd/lambda/getdoc/getdoc.go
+++ cmd/lambda/getdoc/getdoc.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/tfaller/jsonball"
 	"github.com/tfaller/jsonball/event"
 	"github.com/tfaller/jsonball/internal/operation"
 	"github.com/tfaller/jsonball/internal/startup"
@@ -14,8 +16,13 @@ var (
 	registry = startup.MustGetRegistry()
 )
 
-func lambdaMain(ctx context.Context, e event.GetDocument) (event.Document, error) {
-	return operation.GetDocumentContent(ctx, registry, e.Type, e.Name)
+func lambdaMain(ctx context.Context, e event.GetDocument) (doc event.Document, err error) {
+	doc, err = operation.GetDocumentContent(ctx, registry, e.Type, e.Name)
+	if errors.Is(err, jsonball.ErrDocumentNotExist) {
+		// return empty document
+		return event.Document{Type: e.Type, Name: e.Name}, nil
+	}
+	return
 }
 
 func main() {
