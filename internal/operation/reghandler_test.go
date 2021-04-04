475f02afc3468a974da26bc8e2239af0fac01e49
Thomas Faller <tfaller1@gmx.de>
2021-04-04 23:15:41 +0200

    Improved test code coverage.

--- internal/operation/reghandler_test.go
+++ internal/operation/reghandler_test.go
@@ -19,14 +19,27 @@ func TestRegisterHandler(t *testing.T) {
 	registry := mock_jsonball.NewMockRegistry(ctrl)
 	detector := mock_propchange.NewMockDetector(ctrl)
 
+	errCommit := errors.New("commit failed")
+	errSetProp := errors.New("set property failed")
+	errOpenDoc := errors.New("open doc failed")
+
 	testCases := []struct {
-		Handler *event.RegisterHandler
-		Error   error
+		Handler    *event.RegisterHandler
+		Error      error
+		ErrCommit  error
+		ErrSetProp error
+		ErrOpenDoc error
 	}{
 		// simple test case
-		{&event.RegisterHandler{Handler: "handler", QueueURL: "queueurl"}, nil},
+		{&event.RegisterHandler{Handler: "handler", QueueURL: "queueurl"}, nil, nil, nil, nil},
 		// too long name
-		{&event.RegisterHandler{Handler: "veryveryverylongnameveryveryverylongname", QueueURL: "queueurl"}, name.ErrHandlerNameInvalid},
+		{&event.RegisterHandler{Handler: "veryveryverylongnameveryveryverylongname", QueueURL: "queueurl"}, name.ErrHandlerNameInvalid, nil, nil, nil},
+		// commit failed
+		{&event.RegisterHandler{Handler: "handler", QueueURL: "queueurl"}, errCommit, errCommit, nil, nil},
+		// set requeue property failed
+		{&event.RegisterHandler{Handler: "handler", QueueURL: "queueurl"}, errSetProp, nil, errSetProp, nil},
+		// open document failed
+		{&event.RegisterHandler{Handler: "handler", QueueURL: "queueurl"}, errOpenDoc, nil, nil, errOpenDoc},
 	}
 
 	for idx, test := range testCases {
@@ -34,12 +47,12 @@ func TestRegisterHandler(t *testing.T) {
 
 		// requeue property must be set
 		docOps := mock_propchange.NewMockDocumentOps(ctrl)
-		docOps.EXPECT().SetProperty(name.HandlerRequeueProperty, gomock.Any())
-		docOps.EXPECT().Commit()
+		docOps.EXPECT().SetProperty(name.HandlerRequeueProperty, gomock.Any()).Return(test.ErrSetProp)
+		docOps.EXPECT().Commit().Return(test.ErrCommit)
 		docOps.EXPECT().Close()
 
 		detector.EXPECT().OpenDocument(ctx, name.CreateDocName(name.HandlerDocumentType, test.Handler.Handler)).Return(
-			docOps, nil,
+			docOps, test.ErrOpenDoc,
 		)
 
 		err := RegisterHandler(ctx, registry, detector, test.Handler)
